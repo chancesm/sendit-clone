22d1391a1bd9ae58e06bbae0b3059b9bb14331b8
Chance Murray <chance.s.murray@gmail.com>
2023-06-11 06:12:32 +0000

    start tunnel service creation

--- main.go
+++ main.go
@@ -4,57 +4,45 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"math"
-	"math/rand"
 	"net/http"
 	"strconv"
 
 	"github.com/gliderlabs/ssh"
+
+	"github.com/chancesm/sendit-clone/services/tunnel"
 )
-
-type Tunnel struct {
-	writer io.Writer
-	donech chan struct{}
-}
-
-// TODO: Make async safe (lock/mutex/etc)
-var tunnels = map[int]chan Tunnel{}
 
 const host = "http://localhost:1337"
 
 func main() {
+
+	ts := tunnel.NewTunnelService()
+	ts.Init()
+
 	go func() {
 		http.HandleFunc("/", handleRequest)
 		log.Fatal(http.ListenAndServe(":1337", nil))
 	}()
 
 	ssh.Handle(func(s ssh.Session) {
-		// TODO: Use some sort of UUID shortcode library
-		id := rand.Intn(math.MaxInt)
 
-		// Create a channel that the http handler will write to
-		tunnels[id] = make(chan Tunnel)
+		tunnelchan, id := ts.MakeTunnelChannel()
 
 		fmt.Printf("Tunnel ID: %d\n", id)
 		s.Write([]byte(fmt.Sprintf("File accessed at: %s/?id=%d\n", host, id)))
 		s.Write([]byte("Waiting for http connection...\n"))
 
 		// Block until the http handler sends a Tunnel back
-		tunnel := <-tunnels[id]
+		tnl := <-tunnelchan
 		fmt.Printf("Tunnel[%d] received by ssh server\n", id)
 
 		// Write the bytes that were passed into the ssh session to the htttp response writer
-		_, err := io.Copy(tunnel.writer, s)
+		_, err := io.Copy(tnl.Writer, s)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// Let the http handler know that we are done
-		close(tunnel.donech)
-
-		// Cleanup channels and tunnels
-		close(tunnels[id])
-		delete(tunnels, id)
+		ts.Cleanup(&tnl, id)
 
 		s.Write([]byte("Success!!!\n"))
 
@@ -64,19 +52,21 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	ts := tunnel.NewTunnelService()
+
 	idstr := r.URL.Query().Get("id")
 	id, _ := strconv.Atoi(idstr)
 
-	tunnel, ok := tunnels[id]
-	if !ok {
-		w.Write([]byte("Tunnel Not Found"))
-		return
-	}
+	tnlchan := ts.GetTunnel(id)
+	// if !ok {
+	// 	w.Write([]byte("Tunnel Not Found"))
+	// 	return
+	// }
 
 	donech := make(chan struct{})
-	tunnel <- Tunnel{
-		writer: w,
-		donech: donech,
+	tnlchan <- tunnel.Tunnel{
+		Writer:   w,
+		DoneChan: donech,
 	}
 	<-donech
 }
---
+++ services/tunnel/tunnel.go
@@ -0,0 +1,50 @@
+package tunnel
+
+import (
+	"io"
+	"math"
+	"math/rand"
+)
+
+type Tunnel struct {
+	Writer   io.Writer
+	DoneChan chan struct{}
+}
+
+// TODO: Make async safe (lock/mutex/etc)
+var tunnels map[int]chan Tunnel
+
+type TunnelService struct{}
+
+func NewTunnelService() *TunnelService {
+	return &TunnelService{}
+}
+
+func (t *TunnelService) Init() {
+	tunnels = make(map[int]chan Tunnel)
+}
+
+func (t *TunnelService) GetTunnel(i int) chan Tunnel {
+	return tunnels[i]
+}
+
+func (t *TunnelService) MakeTunnelChannel() (chan Tunnel, int) {
+	// TODO: Use some sort of UUID shortcode library
+	id := rand.Intn(math.MaxInt)
+
+	// Create a channel that the http handler will write to
+	tunnels[id] = make(chan Tunnel)
+
+	return tunnels[id], id
+}
+
+func (t *TunnelService) Cleanup(tnl *Tunnel, id int) {
+
+	// Let the http handler know that we are done
+	close(tnl.DoneChan)
+
+	// Cleanup channels and tunnels
+	close(tunnels[id])
+	delete(tunnels, id)
+
+}
